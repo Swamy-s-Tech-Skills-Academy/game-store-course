27ba70cb0fa21e8304dba5d514bd1e49407fa6b9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-16 14:28:16 +0530

    Code Refactoring

--- src/GameStore.Api/Program.cs
+++ src/GameStore.Api/Program.cs
@@ -1,5 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+WebApplication? app = builder.Build();
+
+// Http Request pipeline Configuration
 
 app.MapGet("/", () => "Hello World!");
 
