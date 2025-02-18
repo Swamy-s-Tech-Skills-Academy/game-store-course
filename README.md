db6cdb8b1a311eee5f6e3bb8190fd0420ac6c7a7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-02-18 15:38:29 +0530

    Swamy/16jan work (#1)
    * Initial Version
    
    * Initial Version
    
    * Update GameStore.sln
    
    * Few Changes
    
    * Code Refactoring
    
    * Update GameStore.Api.csproj
    
    * Create gamestore.http
    
    * Update gamestore.http
    
    * Models
    
    * Added Model and List of Games
    
    * Update Program.cs

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# game-store-course
+# Game Store Solution
+
 I am learning Game Store .NET 8/9 from different Video Courses, Books, and Websites.
+
+## Solution Setup
+
+```powershell
+D:\STSA\game-store-course> dotnet new sln -n GameStore
+
+D:\STSA\game-store-course\src> dotnet new web -n GameStore.Api
+
+PS D:\STSA\game-store-course> dotnet sln add .\src\GameStore.Api\GameStore.Api.csproj
+```
+
+## MSSQLLocalDB Setup
+
+```text
+PS C:\Users\swamy> SqlLocalDB stop MSSQLLocalDB -k
+LocalDB instance "MSSQLLocalDB" stopped.
+PS C:\Users\swamy> SqlLocalDB delete MSSQLLocalDB
+LocalDB instance "MSSQLLocalDB" deleted.
+PS C:\Users\swamy> SqlLocalDB create MSSQLLocalDB
+LocalDB instance "MSSQLLocalDB" created with version 15.0.4382.1.
+PS C:\Users\swamy> SqlLocalDB start MSSQLLocalDB
+LocalDB instance "MSSQLLocalDB" started.
+PS C:\Users\swamy>
+```
---
+++ src/GameStore.Api/Program.cs
@@ -0,0 +1,39 @@
+using GameStore.Api.Models;
+
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+WebApplication? app = builder.Build();
+
+// Http Request pipeline Configuration
+
+
+// Endpoints
+
+List<Game> games =
+[
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "Street Fighter II",
+        Genre = "Fighting",
+        Price = 19.99m,
+        ReleaseDate = new DateOnly(1992, 7, 15)
+    },
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "Final Fantasy XIV",
+        Genre = "Roleplaying",
+        Price = 59.99m,
+        ReleaseDate = new DateOnly(2010, 9, 30) },
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "FIFA 23",
+        Genre = "Sports",
+        Price = 69.99m,
+        ReleaseDate = new DateOnly(2022, 9, 27) }
+];
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
