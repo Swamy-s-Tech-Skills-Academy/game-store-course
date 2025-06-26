6a3da3f60c9460c0b4d7b80998df831bd59291b1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-06-26 22:27:29 +0530

    Add UpdateGameDto for updating game details with validation attributes

--- src/GameStore.Api/Dtos/UpdateGameDto.cs
+++ src/GameStore.Api/Dtos/UpdateGameDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace GameStore.Api.Dtos;
 
-public record CreateGameDto(
+public record UpdateGameDto(
     [Required][StringLength(50)] string Name,
     Guid GenreId,
     [Range(1, 100)] decimal Price,
