76c2af08af0b6d65bfe032787217b9616ee771bc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-06-26 17:14:22 +0530

    Changes (#12)

--- src/GameStore.Api/Models/Game.cs
+++ src/GameStore.Api/Models/Game.cs
@@ -10,12 +10,12 @@ public class Game
     [StringLength(50, MinimumLength = 3, ErrorMessage = "The name must be between 3 and 50 characters long.")]
     public required string Name { get; set; }
 
-    [Required]
-    [StringLength(20, MinimumLength = 3, ErrorMessage = "The genre must be between 3 and 20 characters long.")]
-    public required string Genre { get; set; }
+    public Genre? Genre { get; set; }
 
     [Range(1, 100, ErrorMessage = "The price must be between $1 and $100.")]
     public decimal Price { get; set; }
 
     public DateOnly ReleaseDate { get; set; }
+
+    public required string Description { get; set; }
 }
